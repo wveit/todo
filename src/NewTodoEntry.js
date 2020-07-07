02fc083a01bb8da0a14634f365cba704bdf23d40
Veit Family <veitfamily@Veits-iMac.local>
2020-07-07 15:27:16 -0700

    Make new todo text input clear when adding
    issue-12

--- src/NewTodoEntry.js
+++ src/NewTodoEntry.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useRef } from "react";
 
 export default function NewTodoEntry(props) {
+  const textInput = useRef(null);
 
-    function handleOnClick() {
-        if(props.onAddTodo)
-            props.onAddTodo(document.querySelector(".todo-entry-input").value);
-    }
+  function handleOnClick() {
+    if (props.onAddTodo)
+      props.onAddTodo(document.querySelector(".todo-entry-input").value);
 
-    return (
-        <div>
-            <input className="todo-entry-input" />
-            <button onClick={handleOnClick}>Add Todo</button>
-        </div>
-    );
+    textInput.current.value = "";
+  }
+
+  return (
+    <div>
+      <input className="todo-entry-input" ref={textInput} />
+      <button onClick={handleOnClick}>Add Todo</button>
+    </div>
+  );
 }
