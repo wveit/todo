62c41d10b34485e080a9d99f49aa541255dac028
Veit Family <veitfamily@Veits-iMac.local>
2020-07-07 18:46:52 -0700

    Enter key should add todo
    issue-13

--- src/NewTodoEntry.js
+++ src/NewTodoEntry.js
@@ -3,17 +3,27 @@ import React, { useRef } from "react";
 export default function NewTodoEntry(props) {
   const textInput = useRef(null);
 
-  function handleOnClick() {
+  function handleAddTodo() {
+    if (textInput.current.value === "") return;
+
     if (props.onAddTodo)
       props.onAddTodo(document.querySelector(".todo-entry-input").value);
 
     textInput.current.value = "";
   }
 
+  function handleKeyPress(event) {
+    if (event.key === "Enter") handleAddTodo();
+  }
+
   return (
     <div>
-      <input className="todo-entry-input" ref={textInput} />
-      <button onClick={handleOnClick}>Add Todo</button>
+      <input
+        className="todo-entry-input"
+        ref={textInput}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
     </div>
   );
 }
